3c6315ff269c6c061d72a8a62f4356941ad04844
Jussi Saurio <jussi.saurio@gmail.com>
2016-11-18 01:08:21 +0200

    pelitoiminnallisuuksia lisätty

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ var peli = require('./peli');
 
 //var bodyParser = require('body-parser');
 //var urlEncParser = bodyParser.urlencoded({ extended: false});
-var peliKaynnissa = false;
-var botName ="akroBotti";
+var pelivaihe ="ei";
+var botName ="AKROBOT";
 var alkukirjaimet =[];
 var akronyymi="";
 var users=[];
+var vote=[];
+var kierrospisteet=[];
+var tulokset={};
 var animals = ["Monkey", "Zebra", "Pigeon", "Hamster", "Kookaburra", "Chimp", "Dog", "Hippo", "Rhinoceros"];
+var vastaukset=[];
+var kierros=0;
+var aika=0;
 
 app.set('view engine', 'ejs') // set the express view engine to ejs (embedded javascript & html)
 app.use('/styles', express.static('styles')); // route requests to styles folder (html file requests css files)
@@ -22,50 +28,6 @@ app.use('/styles', express.static('styles')); // route requests to styles folder
 app.get('/', function (req, res) {
 	res.render('index');
 });
-
-
-function aloitaPeli () {
-
-		var kierros=1;
-		io.emit('viesti', {username: botName, msg: "Kierros " + kierros});
-
-		var akrPituus = Math.round(Math.random()*4+3);
-		
-		for (var k=0; k < akrPituus; k++) {
-
-		alkukirjaimet.push(String.fromCharCode(Math.floor(Math.random()*22 + 65)));
-		}
-		akronyymi = alkukirjaimet.join('');
-
-		io.emit('akronyymi', akronyymi);
-
-		var aika=10;
-
-		var sekuntikello = setInterval(function() {
-
-			io.emit('kello', aika);
-			aika--;
-
-			if (aika<0) {
-
-				clearInterval(sekuntikello);
-				io.emit('kello', '');
-			}
-		}, 1000);
-	}
-
-
-function kasitteleVastaus (vastaus, pelaaja) {
-
-	var validi = true;
-
-	for (var s=0; s < vastaus.length; s++) {
-
-		if (vastaus[s][0].toUpperCase() !== alkukirjaimet[s]) validi = false;
-	}
-
-	if (!validi) {io.emit('viesti', {username: botName, msg: pelaaja+", annoit virheellisen vastauksen. Kokeile uudelleen!"})}
-}
 
 
 
@@ -78,7 +40,7 @@ io.on('connection', function(socket) {
 		users[socket.id] = "Anonymous" + animals[Math.floor(Math.random()*animals.length)] + Math.floor(Math.random()*100);
 				var data = {username: users[socket.id]};
 		var welcomeData = {username: botName, msg: "Vitun " + users[socket.id] + " tervetuloo tsättäILEEEN, tän pitäs näkyy sulle vaa privana ;)"};
-		socket.emit('viesti', welcomeData);
+		socket.emit('privaviesti', welcomeData);
 	});
 	
 
@@ -104,21 +66,28 @@ io.on('connection', function(socket) {
 				io.emit('viesti', v);
 			}
 
-			if (msgArray[0] === "/aloita" && msgArray.length <= 2 && !peliKaynnissa) {
+			if (msgArray[0] === "/aloita" && msgArray.length <= 2 && pelivaihe=="ei") {
 
-				peliKaynnissa=true;
-				v.msg ="Nöniin, käyttäjä " + v.username +" haluaa pelata akronyymipeli PASKAa. Luvassa 5 kierrosta. "
+				pelivaihe ="intro";
+				v.msg ="Nöniin, käyttäjä " + users[socket.id] +" haluaa pelata akronyymipeli PASKAa. Luvassa 5 kierrosta. "
 				if (msgArray[1] && msgArray[1].toLowerCase() === "säännöt") v.msg += "Pelin tarkoituksena on keksiä mahdollisimman helmi lause, fraasi tahi vastaava annetusta akronyymista. Esim. akronyymista JEES keksi vaikka Jeesus Elää Emmoo Sienipäissäni. =D ";
 				v.msg += "Aloitetaan pian...";
-				v.username = peli.botName;
+				v.username = botName;
 				io.emit('viesti', v); //
 
-				setTimeout(aloitaPeli, 5000);
+				setTimeout(aloitaPeli, 2000);
 			}
 
-			if (msgArray[0] === "/paska" && msgArray.length > 1 && peliKaynnissa) {
+			if (msgArray[0] === "/v" && msgArray.length > 1 && pelivaihe === "laadinta") {
 
-				var vastaus = msgArray.splice(0,1);
+				var vastaus = msgArray.slice(1);
+
+				kasitteleVastaus(vastaus, v.username);
+			}
+
+			if (msgArray[0] === "/v" && msgArray.length === 2 && pelivaihe === "aanestys") {
+
+				var vastaus = msgArray.slice(1);
 
 				kasitteleVastaus(vastaus, v.username);
 			}
@@ -132,6 +101,193 @@ io.on('connection', function(socket) {
 		delete users[socket.id];
 
 	});
+
+
+
+
+	function aloitaPeli () {
+
+		if (kierros===0) tulokset={};
+		if (kierros >=5) {pelivaihe="ei"; return;}
+
+		pelivaihe="laadinta";
+		alkukirjaimet =[];
+		vastaukset=[];
+		vote=[];
+		akronyymi="";
+		kierros++;
+		kierrospisteet[kierros]=[];
+		
+
+		var akrPituus = Math.round(Math.random()*4+3);
+		
+		for (var k=0; k < akrPituus; k++) {
+
+		alkukirjaimet.push(String.fromCharCode(Math.floor(Math.random()*22 + 65)));
+		}
+		akronyymi = alkukirjaimet.join('');
+
+		io.emit('boldviesti', {username: botName, msg: "Kierros " + kierros +": " +akronyymi});
+		io.emit('akronyymi', "Akronyymi: " + akronyymi);
+
+		aika=5;
+
+		var sekuntikello = setInterval(function() {
+
+			io.emit('kello', aika);
+			aika--;
+
+			if (aika<0) {
+
+				clearInterval(sekuntikello);
+				io.emit('kello', '');
+				io.emit('akronyymi', '')
+				pelivaihe="aanestys";
+				listaaVastaukset();
+			}
+		}, 1000);
+	}
+
+function listaaVastaukset() {
+
+	if (vastaukset.length===0) { 
+		io.emit('viesti', {username: botName, msg: "Pelakkaa ny saatana"});
+		if (kierros <5) aloitaPeli();
+		else pelaajaPisteet();
+
+		return;
+	}
+	for (var a=0; a < vastaukset.length; a++) {
+
+		io.emit('boldviesti', {username: botName, msg: (a+1)+". " + vastaukset[a].vastaus});
+	}
+
+	io.emit('viesti', {username: botName, msg: "Äänestä suosikkiasi komennolla /v [nro], esim. /v 666"});
+
+	pelivaihe ="aanestys";
+	io.emit('akronyymi', "Äänestä nyt");
+
+	for (var k=0; k < vastaukset.length; k++) {
+		kierrospisteet[kierros][k]=0;
+	}
+	
+	aika=5;
+
+		var sekuntikello = setInterval(function() {
+
+			io.emit('kello', aika);
+			aika--;
+
+			if (aika<0) {
+
+				clearInterval(sekuntikello);
+				io.emit('kello', '');
+				pelivaihe="kierrostulokset";
+				kierrosTulokset();
+			}
+		}, 1000);
+}
+
+function kierrosTulokset() {
+
+
+	for (var t=0; t < vastaukset.length; t++) {
+		io.emit('boldviesti', {username: botName, msg: vastaukset[t].pelaaja +": " + vastaukset[t].vastaus + " - " + kierrospisteet[kierros][t] +"p."});
+	}
+	setTimeout(pelaajaPisteet, 2000);
+	setTimeout(aloitaPeli, 5000);
+}
+
+function pelaajaPisteet() {
+
+	if (kierros <=4) {
+	io.emit('boldviesti', {username: botName, msg: "Välitulokset:"});
+
+	console.log(Object.keys(tulokset));
+	}
+	
+	if (kierros===5) {
+	io.emit('boldviesti', {username: botName, msg: "Lopputulokset:"});
+
+	console.log(Object.keys(tulokset));
+
+	}
+	
+	var results=[];
+	
+	for (pelaaja in tulokset) {
+
+		results.push([tulokset[pelaaja], pelaaja]);
+		results = results.sort(function(a,b) {
+
+			return b[0] - a[0];
+		});
+	}
+
+	results.forEach(function (r){
+		io.emit('boldviesti', {username: botName, msg: r[1] +": " + r[0] +"p."});
+	});
+	
+
+
+	io.emit('boldviesti', {username: botName, msg: ''});
+
+	if (kierros===5) {pelivaihe="ei"; kierros=0; vastaukset={};}
+}
+
+function kasitteleVastaus (vastaus, pelaaja) {
+
+	var validi = true;
+
+	if (pelivaihe==="laadinta") {
+		
+		if (vastaus.length !== alkukirjaimet.length) validi=false;
+		else {
+		for (var s=0; s < alkukirjaimet.length; s++) {
+
+			console.log (vastaus[s][0].toUpperCase() + " ja " + alkukirjaimet[s]);
+			if (vastaus[s][0].toUpperCase() !== alkukirjaimet[s]) validi = false;
+		}
+		}
+
+		if (!validi) {
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": annoit virheellisen vastauksen. Kokeile uudelleen!"});
+		}
+		else {
+
+			vastaukset.push({pelaaja: pelaaja, vastaus: vastaus.join(' ')});
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": OK."});
+		}
+	}
+	else if (pelivaihe==="aanestys") {
+
+		if (isNaN(vastaus) || !vastaukset[vastaus-1]) {
+
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": äänestit virheellisesti. Kokeile uudelleen!"});
+		}
+		else if (vote[socket.id] === true) {
+
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": et voi äänestää monta kertaa. Homo."});
+		}
+		else if (vastaukset[vastaus-1].pelaaja === users[socket.id]) {
+
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": et voi äänestää ITTEES vitun pelle."});
+
+		}
+		else {
+
+			vote[socket.id] = true;
+			kierrospisteet[kierros][vastaus-1]++;
+			console.log (tulokset);
+
+			if (!tulokset.hasOwnProperty(vastaukset[vastaus-1].pelaaja)) tulokset[vastaukset[vastaus-1].pelaaja]=1;
+			else tulokset[vastaukset[vastaus-1].pelaaja]++;
+
+			socket.emit('privaviesti', {username: botName, msg: pelaaja+": OK."});
+		}
+	}
+}
+
 
 
 	
